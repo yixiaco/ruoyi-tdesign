edcbec3d4ea80faf521ffbf831611c33d3e84244
yixiaco <627428179@qq.com>
2023-04-27 16:25:16 +0800

    feat redis工具扩展

--- ruoyi-common/ruoyi-common-redis/src/main/java/org/dromara/common/redis/utils/RedisLockUtil.java
+++ ruoyi-common/ruoyi-common-redis/src/main/java/org/dromara/common/redis/utils/RedisLockUtil.java
@@ -2,6 +2,9 @@ package org.dromara.common.redis.utils;
 
 import org.redisson.api.RLock;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 import java.util.function.Supplier;
@@ -19,25 +22,88 @@ public class RedisLockUtil {
      *
      * @param key             缓存key
      * @param defaultSupplier 不存在值时回调
-     * @param <T>
-     * @return
      */
     public static <T> T getOrSave(String key, Supplier<T> defaultSupplier) {
         return getOrAfter(key, null, null, defaultSupplier, RedisUtils::setCacheObject);
     }
 
     /**
-     * 获取并保存到缓存中
+     * 获取并保存到缓存中，设置获取锁后保留锁的最长时间
      *
      * @param key             缓存key
      * @param leaseTime       如果尚未通过调用unlock释放锁，则在获取锁后保留锁的最长时间。如果leaseTime为-1，则保持锁定直到显式解锁
      * @param unit            时间单位
      * @param defaultSupplier 不存在值时回调
-     * @param <T>
-     * @return
      */
     public static <T> T getOrSave(String key, Long leaseTime, TimeUnit unit, Supplier<T> defaultSupplier) {
         return getOrAfter(key, leaseTime, unit, defaultSupplier, RedisUtils::setCacheObject);
+    }
+
+    /**
+     * 获取并保存到缓存中
+     *
+     * @param key             缓存key
+     * @param defaultSupplier 不存在值时回调
+     */
+    public static <T> List<T> getOrSaveList(String key, Supplier<List<T>> defaultSupplier) {
+        return getOrAfter(key, null, null, defaultSupplier, RedisUtils::setCacheList);
+    }
+
+    /**
+     * 获取并保存到缓存中，设置指定获取锁后保留锁的最长时间
+     *
+     * @param key             缓存key
+     * @param leaseTime       如果尚未通过调用unlock释放锁，则在获取锁后保留锁的最长时间。如果leaseTime为-1，则保持锁定直到显式解锁
+     * @param unit            时间单位
+     * @param defaultSupplier 不存在值时回调
+     */
+    public static <T> List<T> getOrSaveList(String key, Long leaseTime, TimeUnit unit, Supplier<List<T>> defaultSupplier) {
+        return getOrAfter(key, leaseTime, unit, defaultSupplier, RedisUtils::setCacheList);
+    }
+
+
+    /**
+     * 获取并保存到缓存中
+     *
+     * @param key             缓存key
+     * @param defaultSupplier 不存在值时回调
+     */
+    public static <T> Set<T> getOrSaveSet(String key, Supplier<Set<T>> defaultSupplier) {
+        return getOrAfter(key, null, null, defaultSupplier, RedisUtils::setCacheSet);
+    }
+
+    /**
+     * 获取并保存到缓存中，设置指定获取锁后保留锁的最长时间
+     *
+     * @param key             缓存key
+     * @param leaseTime       如果尚未通过调用unlock释放锁，则在获取锁后保留锁的最长时间。如果leaseTime为-1，则保持锁定直到显式解锁
+     * @param unit            时间单位
+     * @param defaultSupplier 不存在值时回调
+     */
+    public static <T> Set<T> getOrSaveSet(String key, Long leaseTime, TimeUnit unit, Supplier<Set<T>> defaultSupplier) {
+        return getOrAfter(key, leaseTime, unit, defaultSupplier, RedisUtils::setCacheSet);
+    }
+
+    /**
+     * 获取并保存到缓存中
+     *
+     * @param key             缓存key
+     * @param defaultSupplier 不存在值时回调
+     */
+    public static <T> Map<String, T> getOrSaveMap(String key, Supplier<Map<String, T>> defaultSupplier) {
+        return getOrAfter(key, null, null, defaultSupplier, RedisUtils::setCacheMap);
+    }
+
+    /**
+     * 获取并保存到缓存中，设置指定获取锁后保留锁的最长时间
+     *
+     * @param key             缓存key
+     * @param leaseTime       如果尚未通过调用unlock释放锁，则在获取锁后保留锁的最长时间。如果leaseTime为-1，则保持锁定直到显式解锁
+     * @param unit            时间单位
+     * @param defaultSupplier 不存在值时回调
+     */
+    public static <T> Map<String, T> getOrSaveMap(String key, Long leaseTime, TimeUnit unit, Supplier<Map<String, T>> defaultSupplier) {
+        return getOrAfter(key, leaseTime, unit, defaultSupplier, RedisUtils::setCacheMap);
     }
 
     /**
@@ -48,8 +114,6 @@ public class RedisLockUtil {
      * @param unit            时间单位
      * @param defaultSupplier 不存在值时回调
      * @param after           没有缓存时执行的回调
-     * @param <T>
-     * @return
      */
     public static <T> T getOrAfter(String key, Long leaseTime, TimeUnit unit, Supplier<T> defaultSupplier, BiConsumer<String, T> after) {
         T data = RedisUtils.getCacheObject(key);
--- ruoyi-common/ruoyi-common-redis/src/main/java/org/dromara/common/redis/utils/RedisReadWriteLockUtil.java
+++ ruoyi-common/ruoyi-common-redis/src/main/java/org/dromara/common/redis/utils/RedisReadWriteLockUtil.java
@@ -3,7 +3,11 @@ package org.dromara.common.redis.utils;
 import org.redisson.api.RLock;
 import org.redisson.api.RReadWriteLock;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
@@ -28,6 +32,42 @@ public class RedisReadWriteLockUtil {
      */
     public static <T> T readWrite(String key, Supplier<T> defaultSupplier) {
         return readWrite(key, defaultSupplier, RedisUtils::setCacheObject);
+    }
+
+    /**
+     * 同时执行读与写操作，当读取值不存在时，执行默认回调获取值
+     *
+     * @param key             key
+     * @param defaultSupplier 默认值回调
+     * @param <T>             数据类型
+     * @return 返回数据
+     */
+    public static <T> List<T> readWriteList(String key, Supplier<List<T>> defaultSupplier) {
+        return readWrite(key, defaultSupplier, RedisUtils::setCacheList);
+    }
+
+    /**
+     * 同时执行读与写操作，当读取值不存在时，执行默认回调获取值
+     *
+     * @param key             key
+     * @param defaultSupplier 默认值回调
+     * @param <T>             数据类型
+     * @return 返回数据
+     */
+    public static <T> Set<T> readWriteSet(String key, Supplier<Set<T>> defaultSupplier) {
+        return readWrite(key, defaultSupplier, RedisUtils::setCacheSet);
+    }
+
+    /**
+     * 同时执行读与写操作，当读取值不存在时，执行默认回调获取值
+     *
+     * @param key             key
+     * @param defaultSupplier 默认值回调
+     * @param <T>             数据类型
+     * @return 返回数据
+     */
+    public static <T> Map<String, T> readWriteMap(String key, Supplier<Map<String, T>> defaultSupplier) {
+        return readWrite(key, defaultSupplier, RedisUtils::setCacheMap);
     }
 
     /**
@@ -66,18 +106,61 @@ public class RedisReadWriteLockUtil {
     }
 
     /**
-     * 执行读操作
+     * 执行读取一个Object操作
      *
      * @param key 缓存key
      * @param <T>
      * @return
      */
     public static <T> T read(String key) {
+        return read(key, RedisUtils::getCacheObject);
+    }
+
+    /**
+     * 执行读取一个List操作
+     *
+     * @param key 缓存key
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> readList(String key) {
+        return read(key, RedisUtils::getCacheList);
+    }
+
+    /**
+     * 执行读取一个Set操作
+     *
+     * @param key 缓存key
+     * @param <T>
+     * @return
+     */
+    public static <T> Set<T> readSet(String key) {
+        return read(key, RedisUtils::getCacheSet);
+    }
+
+    /**
+     * 执行读取一个Map操作
+     *
+     * @param key 缓存key
+     * @param <T>
+     * @return
+     */
+    public static <T> Map<String, T> readMap(String key) {
+        return read(key, RedisUtils::getCacheMap);
+    }
+
+    /**
+     * 执行读操作
+     *
+     * @param key 缓存key
+     * @return
+     */
+    public static <T> T read(String key, Function<String, T> after) {
         RReadWriteLock readWriteLock = RedisUtils.getClient().getReadWriteLock("readWriteLock:" + key);
         RLock lock = readWriteLock.readLock();
         lock.lock();
         try {
-            return RedisUtils.getCacheObject(key);
+            return after.apply(key);
         } finally {
             lock.unlock();
         }
@@ -88,8 +171,7 @@ public class RedisReadWriteLockUtil {
      *
      * @param key             缓存key
      * @param defaultSupplier 获取值回调
-     * @param <T>
-     * @return
+     * @return 当前值
      */
     public static <T> T write(String key, Supplier<T> defaultSupplier) {
         return write(key, defaultSupplier, RedisUtils::setCacheObject);
@@ -100,8 +182,40 @@ public class RedisReadWriteLockUtil {
      *
      * @param key             缓存key
      * @param defaultSupplier 获取值回调
+     * @return 当前值
+     */
+    public static <T> List<T> writeList(String key, Supplier<List<T>> defaultSupplier) {
+        return write(key, defaultSupplier, RedisUtils::setCacheList);
+    }
+
+    /**
+     * 执行写操作
+     *
+     * @param key             缓存key
+     * @param defaultSupplier 获取值回调
+     * @return 当前值
+     */
+    public static <T> Set<T> writeSet(String key, Supplier<Set<T>> defaultSupplier) {
+        return write(key, defaultSupplier, RedisUtils::setCacheSet);
+    }
+
+    /**
+     * 执行写操作
+     *
+     * @param key             缓存key
+     * @param defaultSupplier 获取值回调
+     * @return 当前值
+     */
+    public static <T> Map<String, T> writeMap(String key, Supplier<Map<String, T>> defaultSupplier) {
+        return write(key, defaultSupplier, RedisUtils::setCacheMap);
+    }
+
+    /**
+     * 执行写操作
+     *
+     * @param key             缓存key
+     * @param defaultSupplier 获取值回调
      * @param after           执行回调获取值后的操作，例如存入缓存中
-     * @param <T>
      * @return
      */
     public static <T> T write(String key, Supplier<T> defaultSupplier, BiConsumer<String, T> after) {
