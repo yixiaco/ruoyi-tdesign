1402dbcae35a24cb5f3f33338a74592674ea11dc
yixiaco <627428179@qq.com>
2023-05-09 11:15:12 +0800

    feat 提供一个简单的消息队列模块

---
+++ ruoyi-common/ruoyi-common-amqp/src/main/java/org/dromara/amqp/handler/PublisherHandler.java
@@ -0,0 +1,112 @@
+package org.dromara.amqp.handler;
+
+import org.dromara.common.core.transactional.PublisherEvent;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * 处理发布消息
+ *
+ * @author hexm
+ * @date 2023/5/9 10:50
+ */
+public class PublisherHandler {
+
+    /**
+     * 通用的消息队列后缀
+     */
+    public static final String QUEUE = "_QUEUE";
+    @Autowired
+    private PublisherEvent publisherEvent;
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    /**
+     * 提交后发送消息
+     *
+     * @param exchange   交换机
+     * @param routingKey 路由键
+     * @param message    消息
+     */
+    public void commitSend(String exchange, String routingKey, Object message) {
+        publisherEvent.commit(() -> send(exchange, routingKey, message));
+    }
+
+    /**
+     * 提交后发送消息
+     *
+     * @param exchange 交换机
+     * @param message  消息
+     */
+    public void commitSend(String exchange, Object message) {
+        publisherEvent.commit(() -> send(exchange, message));
+    }
+
+    /**
+     * 提交后发送消息
+     *
+     * @param exchange             交换机
+     * @param routingKey           路由键
+     * @param message              消息
+     * @param messagePostProcessor 消息提交时的处理，例如设置延迟队列
+     */
+    public void commitSend(String exchange, String routingKey, Object message, MessagePostProcessor messagePostProcessor) {
+        publisherEvent.commit(() -> send(exchange, routingKey, message, messagePostProcessor));
+    }
+
+    /**
+     * 提交后发送消息
+     *
+     * @param exchange             交换机
+     * @param message              消息
+     * @param messagePostProcessor 消息提交时的处理，例如设置延迟队列
+     */
+    public void commitSend(String exchange, Object message, MessagePostProcessor messagePostProcessor) {
+        publisherEvent.commit(() -> send(exchange, message, messagePostProcessor));
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param exchange   交换机
+     * @param routingKey 路由键
+     * @param message    消息
+     */
+    public void send(String exchange, String routingKey, Object message) {
+        amqpTemplate.convertAndSend(exchange, routingKey, message);
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param exchange 交换机
+     * @param message  消息
+     */
+    public void send(String exchange, Object message) {
+        amqpTemplate.convertAndSend(exchange, exchange + QUEUE, message);
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param exchange             交换机
+     * @param routingKey           路由键
+     * @param message              消息
+     * @param messagePostProcessor 消息提交时的处理，例如设置延迟队列
+     */
+    public void send(String exchange, String routingKey, Object message, MessagePostProcessor messagePostProcessor) {
+        amqpTemplate.convertAndSend(exchange, routingKey, message, messagePostProcessor);
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param exchange             交换机
+     * @param message              消息
+     * @param messagePostProcessor 消息提交时的处理，例如设置延迟队列
+     */
+    public void send(String exchange, Object message, MessagePostProcessor messagePostProcessor) {
+        amqpTemplate.convertAndSend(exchange, exchange + QUEUE, message, messagePostProcessor);
+    }
+}
---
+++ ruoyi-common/ruoyi-common-amqp/src/main/java/org/dromara/amqp/msg/BaseMqMessage.java
@@ -0,0 +1,22 @@
+package org.dromara.amqp.msg;
+
+import cn.hutool.core.util.IdUtil;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 消息基础类，默认分配一个消息id
+ *
+ * @author hexm
+ * @date 2023/5/9 10:50
+ */
+@Data
+public class BaseMqMessage implements Serializable {
+
+    private String msgId;
+
+    public BaseMqMessage() {
+        this.msgId = IdUtil.objectId();
+    }
+}
