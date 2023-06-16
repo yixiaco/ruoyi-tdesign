74b633d2874db044f8dc2f4be9d3f92b0fbe6852
yixiaco <co627428179@gmail.com>
2023-06-16 23:48:31 +0800

    fix：demo发送短信接口实例化失败

--- ruoyi-modules/ruoyi-demo/src/main/java/org/dromara/demo/controller/SmsController.java
+++ ruoyi-modules/ruoyi-demo/src/main/java/org/dromara/demo/controller/SmsController.java
@@ -1,10 +1,8 @@
 package org.dromara.demo.controller;
 
 import org.dromara.common.core.domain.R;
-import org.dromara.sms4j.api.SmsBlend;
+import org.dromara.common.sms.service.SmsService;
 import org.dromara.sms4j.api.entity.SmsResponse;
-import org.dromara.sms4j.core.factory.SmsFactory;
-import org.dromara.sms4j.provider.enumerate.SupplierType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,9 +24,7 @@ import java.util.LinkedHashMap;
 public class SmsController {
 
     @Autowired
-//    private SmsService smsService;
-//    private final SmsTemplate smsTemplate; // 可以使用spring注入
-//    private final AliyunSmsTemplate smsTemplate; // 也可以注入某个厂家的模板工具
+    private SmsService smsService;
 
     /**
      * 发送短信Aliyun
@@ -40,8 +36,7 @@ public class SmsController {
     public R<Object> sendAliyun(String phones, String templateId) {
         LinkedHashMap<String, String> map = new LinkedHashMap<>(1);
         map.put("code", "1234");
-        SmsBlend smsBlend = SmsFactory.createSmsBlend(SupplierType.ALIBABA);
-        SmsResponse smsResponse = smsBlend.sendMessage(phones, templateId, map);
+        SmsResponse smsResponse = smsService.send(phones, templateId, map);
         return R.ok(smsResponse);
     }
 
@@ -56,8 +51,7 @@ public class SmsController {
         LinkedHashMap<String, String> map = new LinkedHashMap<>(1);
 //        map.put("2", "测试测试");
         map.put("1", "1234");
-        SmsBlend smsBlend = SmsFactory.createSmsBlend(SupplierType.TENCENT);
-        SmsResponse smsResponse = smsBlend.sendMessage(phones, templateId, map);
+        SmsResponse smsResponse = smsService.send(phones, templateId, map);
         return R.ok(smsResponse);
     }
 
