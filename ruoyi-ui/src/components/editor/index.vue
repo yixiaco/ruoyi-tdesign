cfdc9a43412a2ee122a790e9e3cc6b48bf6ba3a9
laiwk <1136137090@qq.com>
2023-07-14 17:21:23 +0800

    modify: 原“tinyMce”从cdn获取，调整为从本地获取。 （注：1.语言包请求有问题；2.emoji无法获取暂时取消。）

--- ruoyi-ui/src/components/editor/index.vue
+++ ruoyi-ui/src/components/editor/index.vue
@@ -1,39 +1,114 @@
 <template>
   <div>
     <!--  文档:https://www.tiny.cloud/docs/tinymce/6/-->
-    <editor ref="editorRef" :api-key="apiKey" :initial-value="value" :init="conf" v-bind="$attrs" />
+    <editor ref="editorRef" :api-key="apiKey" :init="conf" v-bind="$attrs" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import 'tinymce/tinymce';
+import 'tinymce/models/dom';
+import 'tinymce/icons/default';
+import 'tinymce/themes/silver/theme';
+// Skins
+import 'tinymce/skins/ui/oxide/skin.min.css';
+import 'tinymce/skins/ui/oxide/content.min.css';
+// Plugins 组件根据init中调用情况自行加载
+import 'tinymce/plugins/preview';
+import 'tinymce/plugins/importcss';
+import 'tinymce/plugins/searchreplace';
+import 'tinymce/plugins/autolink';
+import 'tinymce/plugins/autosave';
+import 'tinymce/plugins/save';
+import 'tinymce/plugins/directionality';
+import 'tinymce/plugins/code';
+import 'tinymce/plugins/visualblocks';
+import 'tinymce/plugins/visualchars';
+import 'tinymce/plugins/fullscreen';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/media';
+import 'tinymce/plugins/codesample';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/charmap';
+import 'tinymce/plugins/pagebreak';
+import 'tinymce/plugins/nonbreaking';
+import 'tinymce/plugins/anchor';
+import 'tinymce/plugins/insertdatetime';
+import 'tinymce/plugins/advlist';
+import 'tinymce/plugins/lists';
+import 'tinymce/plugins/wordcount';
+import 'tinymce/plugins/help';
+import 'tinymce/plugins/quickbars';
+
+// import 'tinymce/plugins/emoticons';
 import Editor from '@tinymce/tinymce-vue';
 import type { RawEditorOptions } from 'tinymce';
-import { ref } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 
-import { SysOssUploadVo } from '@/api/system/model/ossModel';
 import { uploader } from '@/api/system/oss';
+import { useSettingStore } from '@/store';
 
 const apiKey = '';
-const value = ref('');
-const useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
+const settingStore = useSettingStore();
+const useDarkMode = computed(() => settingStore.displayMode === 'dark');
 // 图片上传逻辑
 const uploadHandler: RawEditorOptions['images_upload_handler'] = (blobInfo): Promise<string> => {
   const file = blobInfo.blob();
   const formData = new FormData();
   formData.append('file', file);
-  return uploader(formData).then((res: SysOssUploadVo) => res.data.url);
+  return uploader(formData).then((res) => res.data.url);
 };
-
+const editorRef = ref<any>(null);
+watch(useDarkMode, () => {
+  const reConfig = {
+    skin_url: useDarkMode.value ? '/skins/ui/oxide-dark' : '/skins/ui/oxide',
+    content_css: useDarkMode.value ? '/skins/content/dark/content.min.css' : '/skins/content/default/content.min.css',
+  };
+  // @ts-ignore
+  editorRef.value.rerender(reConfig);
+  removeTinyWarning();
+});
+// 重新渲染时会再次出现提醒，因此单独抽离出来
+function removeTinyWarning() {
+  // 当观察到变动时执行的回调函数
+  function callback(mutationsList: any, observer: any) {
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        const dcm = document.querySelector('.tox-notification__dismiss') as HTMLButtonElement;
+        if (dcm) {
+          dcm.click();
+          observer.disconnect();
+          return;
+        }
+      }
+    }
+  }
+  if (!apiKey) {
+    // 选择需要观察变动的节点
+    const targetNode = document.getElementsByTagName('body')[0];
+    // 观察器的配置（需要观察什么变动）
+    const config = { childList: true, subtree: true };
+    // 创建一个观察器实例并传入回调函数
+    const bodyObserver = new MutationObserver(callback);
+    // 以上述配置开始观察目标节点
+    bodyObserver.observe(targetNode, config);
+  }
+}
+onMounted(() => {
+  removeTinyWarning();
+});
 // 配置项
 const conf: RawEditorOptions = {
-  language: 'zh_CN',
+  promotion: false,
+  language_url: '/langs/zh-Hans.js',
+  language: 'zh-Hans',
   plugins:
-    'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+    'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars',
   editimage_cors_hosts: ['picsum.photos'],
   menubar: 'file edit view insert format tools table help',
   toolbar:
-    'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media link anchor codesample | ltr rtl',
+    'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap | fullscreen  preview save print | insertfile image media link anchor codesample | ltr rtl',
   autosave_ask_before_unload: true,
   autosave_interval: '30s',
   autosave_prefix: '{path}{query}-{id}-',
@@ -50,13 +125,11 @@ const conf: RawEditorOptions = {
     if (meta.filetype === 'file') {
       callback(fileName, { text: 'My text' });
     }
-
     // Provide image and alt text for the image dialog
     if (meta.filetype === 'image') {
       const fileName = 'myimage.jpg';
       callback(fileName, { alt: 'My alt text' });
     }
-
     // Provide alternative source and posted for the media dialog
     if (meta.filetype === 'media') {
       callback(fileName, { source2: 'alt.ogg', poster: 'image.jpg' });
@@ -69,8 +142,11 @@ const conf: RawEditorOptions = {
   noneditable_class: 'mceNonEditable',
   toolbar_mode: 'sliding',
   contextmenu: 'link image table',
-  skin: useDarkMode ? 'oxide-dark' : 'oxide',
-  content_css: useDarkMode ? 'dark' : 'default',
+  skin_url: useDarkMode.value ? '/skins/ui/oxide-dark' : '/skins/ui/oxide',
+  content_css: useDarkMode.value ? '/skins/content/dark/content.min.css' : '/skins/content/default/content.min.css',
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }',
+  // skin: useDarkMode.value ? oxideDark : oxide,
+  // content_css: useDarkMode.value ? 'dark' : 'default',
+  // skin_url: '/skins/ui/oxide',
 };
 </script>
