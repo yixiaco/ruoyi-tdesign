26cb8f3d16571ce82b6d1c24519d6a25936cc2f7
yixiaco <627428179@qq.com>
2023-07-18 15:40:28 +0800

    feat: 新增tinymce富文本预览

---
+++ ruoyi-ui/src/components/editor-preview/index.vue
@@ -0,0 +1,92 @@
+<template>
+  <div ref="html" class="editor_preview" v-html="htmlText"></div>
+</template>
+<script setup lang="ts">
+import 'highlight.js/styles/github.css';
+
+import hljs from 'highlight.js/lib/common';
+import { nextTick, onMounted, onUpdated, ref } from 'vue';
+
+defineProps({
+  htmlText: {
+    type: String,
+    required: true,
+  },
+});
+
+const html = ref<HTMLElement>();
+
+/**
+ * 渲染元素
+ */
+function renderHtml() {
+  nextTick(() => {
+    html.value?.querySelectorAll('pre code')?.forEach((el) => {
+      hljs.highlightElement(el as HTMLElement);
+    });
+  });
+}
+
+onMounted(() => {
+  console.log(hljs.listLanguages());
+  hljs.registerAliases(['markup'], { languageName: 'markdown' });
+  hljs.registerAliases(['csharp'], { languageName: 'c#' });
+  renderHtml();
+});
+onUpdated(() => {
+  renderHtml();
+});
+</script>
+
+<style scoped lang="less">
+.editor_preview :deep {
+  font-family: -apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Oxygen,Ubuntu,Cantarell,'Open Sans','Helvetica Neue',sans-serif;
+  line-height: 1.4;
+  margin: 1rem;
+  table {
+      border-collapse: collapse
+  }
+  table:not([cellpadding]) td,table:not([cellpadding]) th {
+      padding: .4rem
+  }
+  table[border]:not([border="0"]):not([style*=border-width]) td,table[border]:not([border="0"]):not([style*=border-width]) th {
+      border-width: 1px
+  }
+  table[border]:not([border="0"]):not([style*=border-style]) td,table[border]:not([border="0"]):not([style*=border-style]) th {
+      border-style: solid
+  }
+  table[border]:not([border="0"]):not([style*=border-color]) td,table[border]:not([border="0"]):not([style*=border-color]) th {
+      border-color: #ccc
+  }
+  figure {
+      display: table;
+      margin: 1rem auto
+  }
+  figure figcaption {
+      color: #999;
+      display: block;
+      margin-top: .25rem;
+      text-align: center
+  }
+  hr {
+      border-color: #ccc;
+      border-style: solid;
+      border-width: 1px 0 0 0
+  }
+  code {
+      background-color: #e8e8e8;
+      border-radius: 3px;
+      padding: .1rem .2rem
+  }
+  blockquote p:not([dir=rtl]) {
+      border-left: 2px solid #ccc;
+      margin-left: 1.5rem;
+      padding-left: 1rem
+  }
+  blockquote p[dir=rtl] {
+      border-right: 2px solid #ccc;
+      margin-right: 1.5rem;
+      padding-right: 1rem
+  }
+}
+</style>
