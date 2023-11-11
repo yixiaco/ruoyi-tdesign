08fbf27110920dfd46f21c57b16b56ff671aa191
yixiaco <627428179@qq.com>
2023-11-11 12:04:45 +0800

    perf: 菜单支持模糊搜索

--- ruoyi-ui/src/layouts/components/Search.vue
+++ ruoyi-ui/src/layouts/components/Search.vue
@@ -5,17 +5,19 @@
       :class="['header-search', { 'hover-active': isSearchFocus }]"
       placeholder="请输入搜索内容"
       :options="options"
+      :filterable="false"
       :popup-props="{
         overlayInnerClassName: ['search-popup'],
       }"
       @blur="changeSearchFocus(false)"
       @focus="changeSearchFocus(true)"
+      @select="handleClick"
     >
       <template #prefix-icon>
         <search-icon class="icon" size="16" />
       </template>
       <template #option="{ option }">
-        <t-highlight-option :content="option.text" :keyword="searchData" @click="handleClick(option.value)" />
+        <div class="t-select-option__highlight-item" v-html="option.label" />
       </template>
     </t-auto-complete>
   </div>
@@ -36,25 +38,27 @@
       placeholder="输入要搜索内容"
       :autofocus="isSearchFocus"
       :options="options"
+      :filterable="false"
       :popup-props="{
         overlayInnerClassName: ['search-popup'],
       }"
       @blur="changeSearchFocus(false)"
+      @select="handleClick"
     >
       <template #prefix-icon>
         <search-icon size="16" />
       </template>
       <template #option="{ option }">
-        <t-highlight-option :content="option.text" :keyword="searchData" @click="handleClick(option.value)" />
+        <div class="t-select-option__highlight-item" v-html="option.label" />
       </template>
     </t-auto-complete>
   </div>
 </template>
 
 <script lang="ts" setup>
+import Fuse from 'fuse.js';
 import { storeToRefs } from 'pinia';
 import { SearchIcon } from 'tdesign-icons-vue-next';
-import type { HighlightOption as THighlightOption } from 'tdesign-vue-next';
 import { computed, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
@@ -65,6 +69,12 @@ defineProps({
   layout: String,
 });
 
+interface SearchOption {
+  text: string;
+  value: string;
+  label: string;
+}
+
 const permissionStore = usePermissionStore();
 const { defaultRoutes: menuRouters } = storeToRefs(permissionStore);
 const router = useRouter();
@@ -74,23 +84,61 @@ const searchData = ref('');
 const changeSearchFocus = (value: boolean) => {
   isSearchFocus.value = value;
 };
-const menus = computed(() => {
+const menus = computed<SearchOption[]>(() => {
   const newMenuRouters = menuRouters.value as Array<MenuRoute>;
   return getLeftMenus(newMenuRouters)
     .filter((value) => value?.title && !value?.meta?.hidden)
-    .map((value) => ({ text: value.title, value: value.path }));
+    .map((value) => ({ text: value.title, value: value.path, label: value.title }));
 });
+
+const fuse = computed(() => {
+  const options = {
+    includeScore: true,
+    threshold: 0.7,
+    fieldNormWeight: 0.5,
+    includeMatches: true,
+    keys: ['label'],
+  };
+  return new Fuse(menus.value, options);
+});
+
 const options = computed(() => {
   if (!searchData.value) {
     return [];
   }
-  return menus.value;
+  const search = fuse.value.search(searchData.value);
+  return search.map((value) => {
+    const item: SearchOption = { ...value.item };
+    value.matches.forEach((value1) => {
+      const key = value1.key as keyof SearchOption;
+      value1.indices.toReversed().forEach((value2) => {
+        item[key] = replaceWord(item[key], value2[0], value2[1] + 1, (sub) => `<b class="t-is-highlight">${sub}</b>`);
+      });
+    });
+    return item;
+  });
 });
 
+/**
+ * 字符串替换
+ * @param str 字符串
+ * @param startIndex 起始下标
+ * @param endIndex 结束下标（不包含）
+ * @param newWord 替换字符串
+ */
+function replaceWord(str: string, startIndex: number, endIndex: number, newWord: (sub: string) => string) {
+  const s1 = str.substring(0, startIndex);
+  const s2 = str.substring(startIndex, endIndex);
+  const s3 = str.substring(endIndex);
+  return s1 + newWord(s2) + s3;
+}
+
 // 处理跳转
-function handleClick(url: string) {
-  searchData.value = '';
-  router.push(url);
+function handleClick(text: string) {
+  const option = menus.value.find((menu) => menu.text === text);
+  router.push({ path: option.value, force: true }).then(() => {
+    searchData.value = '';
+  });
 }
 
 /** 获取叶子节点的菜单结构 */
@@ -115,13 +163,10 @@ function getLeftMenus(menus: Array<MenuRoute>, parent?: MenuRoute): MenuRoute[] 
   });
 }
 </script>
-<!-- eslint-disable-next-line vue-scoped-css/enforce-style-type -->
-<style lang="less">
-.search-popup {
+<style lang="less" scoped>
+:global(.search-popup) {
   width: auto !important;
 }
-</style>
-<style lang="less" scoped>
 .header-menu-search {
   display: flex;
   margin-left: 16px;
