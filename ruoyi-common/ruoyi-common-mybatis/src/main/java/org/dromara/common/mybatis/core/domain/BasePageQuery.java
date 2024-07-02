2287ec667060e2a365d5789146c699fb9b326910
yixiaco <627428179@qq.com>
2024-07-02 12:02:57 +0800

    feat(validate): 新增用户校验分组并更新页码大小校验规则 新增了UserAddGroup、UserEditGroup和UserQueryGroup三个校验分组接口，用于用户相关操作的验证逻辑区分。同时，更新了BasePageQuery中的校验规则，将页码和每页大小的校验增加了UserQueryGroup分组，以便进行更细致的验证控制。

--- ruoyi-common/ruoyi-common-mybatis/src/main/java/org/dromara/common/mybatis/core/domain/BasePageQuery.java
+++ ruoyi-common/ruoyi-common-mybatis/src/main/java/org/dromara/common/mybatis/core/domain/BasePageQuery.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.dromara.common.core.validate.QueryGroup;
+import org.dromara.common.core.validate.UserQueryGroup;
 
 /**
  * 基础分页查询
@@ -20,15 +21,15 @@ public class BasePageQuery extends BaseEntity {
     /**
      * 页码
      */
-    @NotNull(message = "页码不能为空", groups = QueryGroup.class)
-    @Min(value = 0, message = "页码不能小于0", groups = QueryGroup.class)
+    @NotNull(message = "页码不能为空", groups = {QueryGroup.class, UserQueryGroup.class})
+    @Min(value = 0, message = "页码不能小于0", groups = {QueryGroup.class, UserQueryGroup.class})
     private Integer pageNum;
 
     /**
      * 每页大小
      */
-    @NotNull(message = "每页大小不能为空", groups = QueryGroup.class)
-    @Min(value = 1, message = "每页大小不能小于1", groups = QueryGroup.class)
-    @Max(value = 100, message = "每页大小不能大于100", groups = QueryGroup.class)
+    @NotNull(message = "每页大小不能为空", groups = {QueryGroup.class, UserQueryGroup.class})
+    @Min(value = 1, message = "每页大小不能小于1", groups = {QueryGroup.class, UserQueryGroup.class})
+    @Max(value = 100, message = "每页大小不能大于100", groups = {QueryGroup.class, UserQueryGroup.class})
     private Integer pageSize;
 }
